4b0295ab76d2c1dcc181f6b91c2441dd3fbfde6f
mkongojr <mmkongo22@gmail.com>
2025-01-03 14:45:10 +0300

    function 1.

---
+++ crud.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+// product structure
+struct product
+{
+    int id;
+    string name;
+    double price;
+
+    // display product details
+    void display()
+    {
+        cout << "ID:" << id << "\nname:" << name << "\nprice:" << price << "\n\n";
+    }
+};
+//function prototypes
+void createProduct();
+void readProduct();
+void updateProduct();
+void deleteProduct();
+vector<product> loadProducts();
+void saveProducts(const vector<product> &products);
+const string filename = "products.txt";
+int main(){
+    int choice;
+    do{
+        cout << "1.create product\n";
+        cout << "2.read product\n";
+        cout << "3.update product\n";
+        cout << "4.delete poduct\n";
+        cout << "exiting....\n";
+        cout << "enter your choice:";
+        cin >> choice;
+        switch(choice){
+        case 1:
+            createProduct();
+        case 2:
+            readProduct();
+            case 3:
+            updateProduct();
+            case 4:
+            deleteProduct();
+            case 5:
+                cout << "exiting....\n";
+                default:
+                    cout << "invalid choice!!try again\n";
+        }
+    } while(choice != 5);
+    return 0;
+
+}
+void createProduct(){
+    vector<product> products = loadProducts();
+    product p;
+    cout << "enter product id:";
+    cin >> p.id;
+    cin.ignore();
+    cout << "enter the product name:";
+    cin >> p.name;
+    cout << "enter the product price:";
+}
